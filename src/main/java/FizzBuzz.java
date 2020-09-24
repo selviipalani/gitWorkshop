b9168c5d721a0849be5b62f8c974f6cf766117cc
Jose <joseeduardocorella@gmail.com>
2020-09-24 16:26:01 -0700

    Trying to pull request from terminal

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class FizzBuzz {
     public static void main(String args[]) {
-        System.out.println("Hello World\n");
+        System.out.println("Hello World");
     }
 }
 
