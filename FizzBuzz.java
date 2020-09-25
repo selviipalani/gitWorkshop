524616346ede3dbdd6230682882245eb646e624f
Jose <joseeduardocorella@gmail.com>
2020-09-25 11:03:17 -0700

    Workshop Prep

---
+++ FizzBuzz.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FizzBuzz {
+    public static void main(String args[]) {
+        for (int i = 0; i <= 100; i++) {
+            if () {
+            } else if () {
+            } else if () {
+            } else {
+            }
+        }
+    }
+}
