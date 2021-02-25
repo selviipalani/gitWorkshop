c9865880908a86c7daedfac22ed9d5423bb0ab71
Jose <joseeduardocorella@gmail.com>
2021-02-24 16:46:30 -0800

    Setting up workshop

---
+++ rust/workshop/target/debug/deps/workshop.d
@@ -0,0 +1,5 @@
+/Users/Gords/git/gitWorkshop/workshop/target/debug/deps/workshop: src/main.rs
+
+/Users/Gords/git/gitWorkshop/workshop/target/debug/deps/workshop.d: src/main.rs
+
+src/main.rs:
---
+++ rust/workshop/target/debug/workshop.d
@@ -0,0 +1,1 @@
+/Users/Gords/git/gitWorkshop/rust/workshop/target/debug/workshop: /Users/Gords/git/gitWorkshop/rust/workshop/src/main.rs
